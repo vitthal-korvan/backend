8a8c8538f1d125b1bafbe498ce00d073f82e1d6f
Vitthal Korvan <korvanvitthal@gmail.com>
2025-08-09 10:10:08 +0530

    update auth.routes.js

--- day 13 - Authentication with JWT/src/routes/auth.routes.js
+++ day 13 - Authentication with JWT/src/routes/auth.routes.js
@@ -17,4 +17,31 @@ router.post('/register', async (req, res)=>{
       })
 })
 
+
+
+router.post('/login', async (req,res)=>{
+      const {username, password} = req.body
+
+      const user = await userModel.findOne({
+            username: username
+      })
+
+      if(!user){
+            return res.status(401).json({
+                  message:"User Not Found [invalid user]"
+            })
+      }
+
+      const isPasswordValid = password == user.password
+       if (!isPasswordValid) {
+         return res.status(401).json({
+           message: "password Not Matching [invalid Password]",
+         });
+       }
+
+       res.status(200).json({
+            message:"User LoggedIn Successfully"
+       })
+})
+
 module.exports= router
