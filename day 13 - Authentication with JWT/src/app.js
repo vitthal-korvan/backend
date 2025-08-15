63ff1624d7d77837c54f76b8a2d86e9d32cc554c
Vitthal Korvan <korvanvitthal@gmail.com>
2025-08-15 08:08:06 +0530

    13, 14 completed!

--- day 13 - Authentication with JWT/src/app.js
+++ day 13 - Authentication with JWT/src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser")
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser())
+
+/*
+
+POST /auth/register
+POST /auth/login
+GET /auth/user
+GET /auth/logout
+
+*/
+
 
 app.use("/auth", authRoutes);
 //we create a routes in which we use for
--- day 13 - Authentication with JWT/src/routes/auth.routes.js
+++ day 13 - Authentication with JWT/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
 
 const router = express.Router()
 
@@ -11,6 +12,10 @@ router.post('/register', async (req, res)=>{
             username, password
       })
 
+      const token = jwt.sign({
+            id:user._id,
+      }, process.env.JWT_SECRET);
+      res.cookie("token", token)
       res.status(201).json({
             message:"User Registered Successfully",
             user
@@ -42,6 +47,36 @@ router.post('/login', async (req,res)=>{
        res.status(200).json({
             message:"User LoggedIn Successfully"
        })
+})
+
+
+router.get("/user", async (req,res)=>{
+      const {token} = req.cookies
+
+      if(!token){
+            return res.status(401).json({
+                  message:"Unauthorized"
+            })
+      }
+
+      try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            // res.send(decoded);
+
+            const user = await userModel.findOne({
+                  _id:decoded.id
+            }).select("-password")
+
+            res.status(200).json({
+                  message:"User Data fetched Successfully",
+                  user
+            })
+      } catch (error) {
+            return res.status(401).json({
+                  message:"Unauthorized"
+            })
+      }
+      
 })
 
 module.exports= router
