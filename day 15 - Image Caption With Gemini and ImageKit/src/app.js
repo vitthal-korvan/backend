b2221c75202a564af6d398609d46ec9b78606692
Vitthal Korvan <korvanvitthal@gmail.com>
2025-09-10 01:21:53 +0530

    day 15 completed

--- day 15 - Image Caption With Gemini and ImageKit/src/app.js
+++ day 15 - Image Caption With Gemini and ImageKit/src/app.js
@@ -1,5 +1,10 @@
 const express = require("express")
-const app = express()
+const authRoutes = require("./routes/auth.routes")
 
+
+const app = express()
+app.use(express.json())
+
+app.use("/auth", authRoutes)
 
 module.exports = app
--- day 15 - Image Caption With Gemini and ImageKit/src/routes/auth.routes.js
+++ day 15 - Image Caption With Gemini and ImageKit/src/routes/auth.routes.js
@@ -1,5 +1,40 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken")
+const router = express.Router();
 
+/*
+ POST - /register
+ POST - /login
+ GET - /users [Protected]
+ 
+ */
 
-module.exports = router
+router.post("/register", async(req, res) => {
+  const { username, password } = req.body;
+
+  const exitingUser = await userModel.findOne({
+      username
+  })
+  if (exitingUser) {
+      return res.status(409).json({
+            message:"Username Already Exist"
+      })
+  }
+
+  const user = await userModel.create({
+      username,password
+  })
+
+  const token = jwt.sign({
+      id:user._id
+  }, process.env.JWT_SECRET)
+
+  res.cookie("token", token)
+  res.status(201).json({
+      message:"User Created Successfully",
+      user
+  })
+});
+
+module.exports = router;
