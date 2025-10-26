0f401a6f24b578dc0c6b5cf8e8d88049bf186c5d
Code Subtle <korvanvitthal@gmail.com>
2025-10-26 13:50:52 +0530

    websocket connection established

--- day 20 - AI ChatBot/server.js
+++ day 20 - AI ChatBot/server.js
@@ -1,9 +1,17 @@
-const express = require('express')
+const app = require("./src/app")
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
 
-const app = express()
+io.on("connection", (socket) => {
+  // ...
+});
 
-app.listen(3000,()=>{
+httpServer.listen(3000, ()=>{
       console.log("Server is running on port 3000");
       
-})
+});
